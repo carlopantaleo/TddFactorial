c651c5665cd36c939e3bf7451d44c56844b9a0fa
Carlo Pantaleo <carlo.pantaleo@lynxspa.com>
2025-02-04 15:54:43 +0100

    Add QwertySortableString

---
+++ FactorialTdd.Tests/QwertySortableStringTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+
+namespace FactorialTdd.Tests;
+
+public class QwertySortableStringTests
+{
+    [Fact]
+    public void Should_ImplicitlyConvertFromString_When_AssignedFromAString()
+    {
+        // Arrange
+        string normalString = "hello";
+
+        // Act
+        QwertySortableString sortableString = normalString;
+
+        // Assert
+        ((string)sortableString).Should().BeEquivalentTo(normalString);
+    }
+
+    [Fact]
+    public void Should_ImplicitlyConvertToString_When_AssignedToAString()
+    {
+        // Arrange
+        QwertySortableString sortableString = "world";
+
+        // Act
+        string normalString = sortableString;
+
+        // Assert
+        normalString.Should().BeEquivalentTo("world");
+    }
+
+    [Fact]
+    public void Should_SortAccordingToQwertyOrder_When_ComparedWithAnotherQwertySortableString()
+    {
+        // Arrange
+        QwertySortableString string1 = "qaz";
+        QwertySortableString string2 = "wsx";
+        QwertySortableString string3 = "edc";
+
+        // Act
+        var result = new List<QwertySortableString> { string3, string1, string2 }
+            .OrderBy(s => s)
+            .ToList();
+
+        // Assert
+        result.Should().ContainInOrder(string1, string2, string3);
+    }
+}
---
+++ FactorialTdd/QwertySortableString.cs
@@ -0,0 +1,24 @@
+﻿namespace FactorialTdd;
+
+public class QwertySortableString
+{
+    private static readonly Dictionary<char, int> QwertyOrder = new();
+
+    static QwertySortableString()
+    {
+        const string qwertyLayout = "qwertyuiopasdfghjklzxcvbnm";
+        for (int i = 0; i < qwertyLayout.Length; i++)
+        {
+            QwertyOrder.Add(qwertyLayout[i], i);
+        }
+    }
+
+    public QwertySortableString(string value)
+    {
+    }
+
+    public static implicit operator QwertySortableString(string value) => throw new NotImplementedException();
+
+    public static implicit operator string(QwertySortableString value) => throw new NotImplementedException();
+
+}
